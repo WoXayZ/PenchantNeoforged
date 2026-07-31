a6fb394bb8a34e900b4e67893f863fd1a68746b9
Clément <woxayz@gmail.com>
2026-07-31 13:44:32 +0200

    1.3

--- src/main/java/archives/tater/penchant/datagen/BookshelfBlockTagGenerator.java
+++ src/main/java/archives/tater/penchant/datagen/BookshelfBlockTagGenerator.java
@@ -2,9 +2,12 @@ package archives.tater.penchant.datagen;
 
 import archives.tater.penchant.Penchant;
 
+import net.minecraft.core.registries.Registries;
 import net.minecraft.core.HolderLookup.Provider;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.Blocks;
 
 import net.neoforged.neoforge.common.Tags;
@@ -22,6 +25,7 @@ public class BookshelfBlockTagGenerator extends BlockTagsProvider {
     protected void addTags(Provider provider) {
         tag(BlockTags.ENCHANTMENT_POWER_PROVIDER)
                 .add(Blocks.CHISELED_BOOKSHELF, Blocks.LECTERN)
-                .addTag(Tags.Blocks.BOOKSHELVES);
+                .addTag(Tags.Blocks.BOOKSHELVES)
+                .addOptionalTag(TagKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath("c", "chiseled_bookshelves")));
     }
 }
