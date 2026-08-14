bfb0fc3e68dd5e44e78fd1f4f27e3c3cc6078c8b
woxayz <woxayz@gmail.com>
2026-08-14 14:16:27 +0200

    1.4

--- src/main/java/archives/tater/penchant/datagen/LootEnchantmentTagGenerator.java
+++ src/main/java/archives/tater/penchant/datagen/LootEnchantmentTagGenerator.java
@@ -1,117 +1,129 @@
-package archives.tater.penchant.datagen;
-
-import archives.tater.penchant.registry.PenchantEnchantmentTags;
-
-import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.EnchantmentTags;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.Enchantments;
-
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-
-public class LootEnchantmentTagGenerator extends PenchantTagsProvider<Enchantment> {
-
-    public static final List<ResourceKey<Enchantment>> UNIQUE = List.of(
-            Enchantments.WIND_BURST, // ominous vault
-            Enchantments.SOUL_SPEED, // bartering/bastion
-            Enchantments.SWIFT_SNEAK // ancient city
-    );
-
-    public static final List<ResourceKey<Enchantment>> RARE = List.of(
-            Enchantments.FROST_WALKER, // igloo
-            Enchantments.FIRE_ASPECT, // nether fortress, ruined portal
-            Enchantments.FLAME, // nether fortress, ruined portal
-            Enchantments.SILK_TOUCH, // mineshaft, dungeon
-            Enchantments.FORTUNE, // mineshaft, dungeon
-            Enchantments.RESPIRATION, // ocean ruins, shipwreck, buried treasure
-            Enchantments.DEPTH_STRIDER, // ocean ruins, shipwreck, buried treasure
-            Enchantments.CHANNELING, // ruins, buried treasure
-            Enchantments.RIPTIDE, // ruins, buried treasure
-            Enchantments.THORNS, // desert temple
-            Enchantments.INFINITY, // jungle temple
-            Enchantments.MULTISHOT // pillager outpost
-    );
-
-    public static final List<ResourceKey<Enchantment>> UNCOMMON = List.of(
-            Enchantments.AQUA_AFFINITY,
-            Enchantments.FEATHER_FALLING,
-            Enchantments.FIRE_PROTECTION,
-            Enchantments.BLAST_PROTECTION,
-            Enchantments.PROJECTILE_PROTECTION,
-            Enchantments.SMITE,
-            Enchantments.BANE_OF_ARTHROPODS,
-            Enchantments.SWEEPING_EDGE,
-            Enchantments.KNOCKBACK,
-            Enchantments.PUNCH,
-            Enchantments.LOOTING,
-            Enchantments.LUCK_OF_THE_SEA
-    );
-
-    public static final List<ResourceKey<Enchantment>> COMMON = List.of(
-            Enchantments.EFFICIENCY,
-            Enchantments.PROTECTION,
-            Enchantments.SHARPNESS,
-            Enchantments.UNBREAKING,
-            Enchantments.POWER,
-            Enchantments.PIERCING,
-            Enchantments.QUICK_CHARGE,
-            Enchantments.IMPALING,
-            Enchantments.LOYALTY,
-            Enchantments.LURE
-    );
-
-    private static ResourceKey<Enchantment> createOptionalId(String namespace, String path) {
+package archives.tater.penchant.datagen;
+
+import archives.tater.penchant.registry.PenchantEnchantmentTags;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.EnchantmentTags;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.Enchantments;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class LootEnchantmentTagGenerator extends PenchantTagsProvider<Enchantment> {
+
+    public static final List<ResourceKey<Enchantment>> UNIQUE = List.of(
+            Enchantments.WIND_BURST, // ominous vault
+            Enchantments.SOUL_SPEED, // bartering/bastion
+            Enchantments.SWIFT_SNEAK // ancient city
+    );
+
+    public static final List<ResourceKey<Enchantment>> RARE = List.of(
+            Enchantments.FROST_WALKER, // igloo
+            Enchantments.FIRE_ASPECT, // nether fortress, ruined portal
+            Enchantments.FLAME, // nether fortress, ruined portal
+            Enchantments.SILK_TOUCH, // mineshaft, dungeon
+            Enchantments.FORTUNE, // mineshaft, dungeon
+            Enchantments.RESPIRATION, // ocean ruins, shipwreck, buried treasure
+            Enchantments.DEPTH_STRIDER, // ocean ruins, shipwreck, buried treasure
+            Enchantments.CHANNELING, // ruins, buried treasure
+            Enchantments.RIPTIDE, // ruins, buried treasure
+            Enchantments.THORNS, // desert temple
+            Enchantments.INFINITY, // jungle temple
+            Enchantments.MULTISHOT // pillager outpost
+    );
+
+    public static final List<ResourceKey<Enchantment>> UNCOMMON = List.of(
+            Enchantments.AQUA_AFFINITY,
+            Enchantments.FEATHER_FALLING,
+            Enchantments.FIRE_PROTECTION,
+            Enchantments.BLAST_PROTECTION,
+            Enchantments.PROJECTILE_PROTECTION,
+            Enchantments.SMITE,
+            Enchantments.BANE_OF_ARTHROPODS,
+            Enchantments.SWEEPING_EDGE,
+            Enchantments.KNOCKBACK,
+            Enchantments.PUNCH,
+            Enchantments.DENSITY,
+            Enchantments.BREACH,
+            Enchantments.LOOTING,
+            Enchantments.LUCK_OF_THE_SEA
+    );
+
+    public static final List<ResourceKey<Enchantment>> COMMON = List.of(
+            Enchantments.EFFICIENCY,
+            Enchantments.PROTECTION,
+            Enchantments.SHARPNESS,
+            Enchantments.UNBREAKING,
+            Enchantments.POWER,
+            Enchantments.PIERCING,
+            Enchantments.QUICK_CHARGE,
+            Enchantments.IMPALING,
+            Enchantments.LOYALTY,
+            Enchantments.LURE,
+            Enchantments.DENSITY
+    );
+
+private static ResourceKey<Enchantment> createOptionalId(String namespace, String path) {
         return ResourceKey.create(Registries.ENCHANTMENT, ResourceLocation.fromNamespaceAndPath(namespace, path));
-    }
-
-    public LootEnchantmentTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+    }
+
+public LootEnchantmentTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
         super(output, Registries.ENCHANTMENT, registriesFuture);
-    }
-
-    @Override
-    protected void addTags(HolderLookup.Provider wrapperLookup) {
-        builder(PenchantEnchantmentTags.UNIQUE)
-                .addAll(UNIQUE);
-
-        builder(PenchantEnchantmentTags.RARE)
-                .addAll(RARE)
-                .addOptional(createOptionalId("veinminer-enchantment", "veinminer"))
-                .addOptional(createOptionalId("veinminer_enchantment", "veinminer"));
-
-        builder(PenchantEnchantmentTags.UNCOMMON)
-                .addAll(UNCOMMON)
-                .addOptional(createOptionalId("farmersdelight", "backstabbing"));
-
-        builder(PenchantEnchantmentTags.COMMON)
-                .addAll(COMMON);
-
-        builder(EnchantmentTags.TREASURE)
-                .addTag(PenchantEnchantmentTags.RARE)
-                .addTag(PenchantEnchantmentTags.UNIQUE);
-        builder(EnchantmentTags.NON_TREASURE)
-                .remove(PenchantEnchantmentTags.RARE)
-                .remove(PenchantEnchantmentTags.UNIQUE);
-        builder(EnchantmentTags.IN_ENCHANTING_TABLE)
-                .remove(PenchantEnchantmentTags.UNCOMMON)
-                .remove(PenchantEnchantmentTags.RARE)
-                .remove(PenchantEnchantmentTags.UNIQUE);
-        builder(EnchantmentTags.TRADEABLE)
-                .remove(PenchantEnchantmentTags.COMMON)
-                .addTag(PenchantEnchantmentTags.UNCOMMON)
-                .remove(PenchantEnchantmentTags.RARE)
-                .remove(PenchantEnchantmentTags.UNIQUE);
-        builder(EnchantmentTags.ON_RANDOM_LOOT)
-                .addTag(PenchantEnchantmentTags.UNCOMMON)
-                .addTag(PenchantEnchantmentTags.RARE)
-                .remove(PenchantEnchantmentTags.UNIQUE);
-        builder(EnchantmentTags.ON_MOB_SPAWN_EQUIPMENT)
-                .addTag(PenchantEnchantmentTags.UNCOMMON)
-                .addTag(PenchantEnchantmentTags.RARE)
-                .remove(PenchantEnchantmentTags.UNIQUE);
-    }
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider wrapperLookup) {
+        builder(PenchantEnchantmentTags.UNIQUE)
+                .addAll(UNIQUE);
+
+        builder(PenchantEnchantmentTags.RARE)
+                .addAll(RARE)
+                .addOptional(createOptionalId("veinminer-enchantment", "veinminer"))
+                .addOptional(createOptionalId("veinminer_enchantment", "veinminer"));
+
+        builder(PenchantEnchantmentTags.UNCOMMON)
+                .addAll(UNCOMMON)
+                .addOptional(createOptionalId("farmersdelight", "backstabbing"));
+
+        builder(PenchantEnchantmentTags.COMMON)
+                .addAll(COMMON);
+
+        builder(EnchantmentTags.TREASURE)
+                .addTag(PenchantEnchantmentTags.RARE)
+                .addTag(PenchantEnchantmentTags.UNIQUE);
+        builder(EnchantmentTags.NON_TREASURE)
+                .remove(PenchantEnchantmentTags.RARE)
+                .remove(PenchantEnchantmentTags.UNIQUE);
+        replaceBuilder(EnchantmentTags.IN_ENCHANTING_TABLE)
+                .addTag(PenchantEnchantmentTags.COMMON);
+        builder(EnchantmentTags.TRADEABLE)
+                .remove(PenchantEnchantmentTags.COMMON)
+                .addTag(PenchantEnchantmentTags.UNCOMMON)
+                .remove(PenchantEnchantmentTags.RARE)
+                .remove(PenchantEnchantmentTags.UNIQUE)
+                .remove(EnchantmentTags.CURSE);
+        builder(EnchantmentTags.ON_TRADED_EQUIPMENT)
+                .addTag(PenchantEnchantmentTags.COMMON)
+                .addTag(PenchantEnchantmentTags.UNCOMMON)
+                .remove(PenchantEnchantmentTags.RARE)
+                .remove(PenchantEnchantmentTags.UNIQUE);
+        builder(EnchantmentTags.ON_RANDOM_LOOT)
+                .addTag(PenchantEnchantmentTags.COMMON)
+                .addTag(PenchantEnchantmentTags.UNCOMMON)
+                .addTag(PenchantEnchantmentTags.RARE)
+                .remove(PenchantEnchantmentTags.UNIQUE);
+        builder(PenchantEnchantmentTags.ON_RANDOM_LOOT_BOOKS)
+                .remove(PenchantEnchantmentTags.COMMON)
+                .remove(EnchantmentTags.CURSE);
+        builder(EnchantmentTags.ON_MOB_SPAWN_EQUIPMENT)
+                .addTag(PenchantEnchantmentTags.COMMON)
+                .addTag(PenchantEnchantmentTags.UNCOMMON)
+                .addTag(PenchantEnchantmentTags.RARE)
+                .remove(PenchantEnchantmentTags.UNIQUE);
+    }
 }
