77b141d9111a3c7312db92ee9970b8f2a2f2643d
woxayz <woxayz@gmail.com>
2026-08-29 00:49:12 +0200

    1.5

--- src/main/java/archives/tater/penchant/client/FontUtils.java
+++ src/main/java/archives/tater/penchant/client/FontUtils.java
@@ -7,6 +7,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.FontDescription;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 
 public class FontUtils {
 
@@ -21,8 +22,9 @@ public class FontUtils {
     public static final Component THIN_SPACE_TEXT = Component.literal(THIN_SPACE).withStyle(style -> style.withFont(FONT));
 
     public static MutableComponent getBar(int width, int progress) {
-        return Component.literal(BAR_SEGMENT.repeat(progress))
-                .append(Component.literal(BAR_SEGMENT.repeat(width - progress))
+        int filled = Mth.clamp(progress, 0, width);
+        return Component.literal(BAR_SEGMENT.repeat(filled))
+                .append(Component.literal(BAR_SEGMENT.repeat(width - filled))
                         .withStyle(ChatFormatting.DARK_GRAY))
                 .withStyle(ChatFormatting.LIGHT_PURPLE)
                 .withStyle(style -> style.withFont(FONT));
