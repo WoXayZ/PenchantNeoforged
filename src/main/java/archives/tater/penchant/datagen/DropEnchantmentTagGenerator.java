3df918743d60afff2ba4364cbc45403f5d08d794
woxayz <woxayz@gmail.com>
2026-09-09 14:44:36 +0200

    1.6

---
+++ src/main/java/archives/tater/penchant/datagen/DropEnchantmentTagGenerator.java
@@ -0,0 +1,23 @@
+package archives.tater.penchant.datagen;
+
+import archives.tater.penchant.registry.PenchantEnchantmentTags;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.item.enchantment.Enchantment;
+
+import java.util.concurrent.CompletableFuture;
+
+public class DropEnchantmentTagGenerator extends PenchantTagsProvider<Enchantment> {
+    public DropEnchantmentTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, Registries.ENCHANTMENT, registriesFuture);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider registries) {
+        // The category tags live in other packs, so they can only be referenced optionally from here.
+        builder(PenchantEnchantmentTags.IGNORE_GUARANTEED_DROP)
+                .addOptionalTag(PenchantEnchantmentTags.COMMON.location());
+    }
+}
